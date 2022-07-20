93ac40f864cfbdd3b33ae1e87d5427eda52fd145
anthony <anthony.aubert1@orange.fr>
2022-07-20 21:16:49 +0200

    Create Royal Family Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import RoyalFamily from "./pages/RoyalFamily";
 import Home from "./pages/Home";
 
 import "./_App.scss";
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/*" element={<Home />} />
+          <Route path="/RoyalFamily" element={<RoyalFamily />} />
         </Routes>
       </div>
     </div>
---
+++ frontend/src/pages/RoyalFamily.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import "../styles/_RoyalFamily.scss";
+
+export default function RoyalFamily() {
+  return (
+    <section className="royal-family">
+      <Header />
+      <div className="container">
+        <h1>fdfjdfhdhfduhghghghghgh</h1>
+      </div>
+    </section>
+  );
+}
