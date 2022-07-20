1adfc8df820c768fe417c655f6c53e10e3360290
anthony <anthony.aubert1@orange.fr>
2022-07-20 17:11:11 +0200

    Create header and Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,25 @@ import "../styles/_Home.scss";
 
 export default function Home() {
   return (
-    <div className="home">
+    <section className="home">
       <Header />
-    </div>
+      <div className="container">
+        <h1>Welcome to the Kingdom of Colchis</h1>
+        <div className="about-container">
+          <h2>About the Kingdom</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit
+            dolores illum perspiciatis eum eveniet hic enim omnis dicta autem
+            assumenda illo, maxime animi accusantium! Perspiciatis voluptatibus
+            fugit iusto, alias iste sapiente rerum enim autem ut id ipsum
+            assumenda dolore deleniti dolorum. Consequatur, magnam fugiat quas
+            reiciendis harum repudiandae ipsam illum ipsa sed voluptatum?
+            Doloremque dicta aliquid illum aut eligendi incidunt voluptate ipsum
+            eveniet voluptatem, praesentium accusantium distinctio culpa quos
+            minima.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
