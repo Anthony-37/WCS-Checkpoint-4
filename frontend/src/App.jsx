dfc4b8dfd7f692b5a5938011d1419848a06dccdf
anthony <anthony.aubert1@orange.fr>
2022-07-22 00:34:29 +0200

    Create Contact Us Page and fixed pb css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import RoyalFamily from "./pages/RoyalFamily";
 import RoyalFamilyDetails from "./pages/RoyalFamilyDetails";
 import RoyalAssets from "./pages/RoyalAssets";
+import ContactUs from "./pages/ContactUs";
 
 import "./_App.scss";
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/RoyalFamily" element={<RoyalFamily />} />
         <Route path="/RoyalFamilyDetails" element={<RoyalFamilyDetails />} />
         <Route path="/RoyalAssets" element={<RoyalAssets />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
       </Routes>
     </div>
   );
